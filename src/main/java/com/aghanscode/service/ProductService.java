fa90fb745f2dac36973e0d4d574f0d1c551c3c32
AghansCode <ganis.ade23@gmail.com>
2022-01-18 21:45:00 +0700

    Create Spring MVC

---
+++ src/main/java/com/aghanscode/service/ProductService.java
@@ -0,0 +1,34 @@
+package com.aghanscode.service;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.aghanscode.entity.Product;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+    
+    private static List<Product> products = new ArrayList<Product>(){
+        {
+            add(new Product(System.currentTimeMillis(), "001", "Product 001", 1000.00));
+            add(new Product(System.currentTimeMillis(), "002", "Product 002", 2000.0));
+            add(new Product(System.currentTimeMillis(), "003", "Product 003", 3000.0));
+            add(new Product(System.currentTimeMillis(), "004", "Product 004", 4000.0));
+            add(new Product(System.currentTimeMillis(), "005", "Product 005", 5000.0));
+        }
+    };
+       
+
+    public List<Product> findAll(){
+        return products;
+    }
+
+    public void addProduct(Product product){
+        product.setId(System.currentTimeMillis());
+        products.add(product);
+    }
+
+}
---
+++ src/test/java/com/aghanscode/demomvc/DemoMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aghanscode.demomvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
